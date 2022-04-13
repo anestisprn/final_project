0e2b3c2a02314659f84961b00a79878772b4224c
rich-ter <me@philipposrichter.com>
2022-04-14 01:10:54 +0300

    changes to the sign_up view and creation of the signup_user html page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,5 @@ from .views import *
 urlpatterns = [
     path('', homepage, name='homepage'),
     # path('login/', login, name='login'),
-
+    path('signup_user', signup_user, name='signup_user'),
 ]
